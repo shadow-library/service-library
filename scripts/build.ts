f4f7c29e3c5158fbf149cf24e12eb362ed558578
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2025-06-05 21:17:41 +0400

    chore: build script (#1)

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,13 +1,13 @@
 /**
  * Importing npm packages
  */
-import { spawnSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 
 /**
  * Importing user defined packages
  */
+import { utils } from '@shadow-library/common';
 
 /**
  * Defining types
@@ -16,6 +16,7 @@ import path from 'path';
 /**
  * Declaring the constants
  */
+const startTime = process.hrtime();
 const rootDir = path.join(import.meta.dirname, '..');
 const distDir = path.join(rootDir, 'dist');
 
@@ -27,52 +28,26 @@ const error = (message: string) => (console.error('\x1b[31m%s\x1b[0m', message),
 if (fs.existsSync(distDir)) fs.rmSync(distDir, { recursive: true });
 fs.mkdirSync(distDir);
 
-const startTime = process.hrtime();
+/** modifying package.json and saving to 'dist' */
 const packageJsonPath = path.join(rootDir, 'package.json');
 const packageJsonString = fs.readFileSync(packageJsonPath, 'utf-8');
 const packageJson = JSON.parse(packageJsonString);
-
-/** modifying package.json and saving to 'dist' */
-const distPackageJson = structuredClone(packageJson);
-distPackageJson.main = './cjs/index.js';
-distPackageJson.module = './esm/index.js';
-distPackageJson.types = './esm/index.d.ts';
-distPackageJson.exports = { '.': { import: './esm/index.js', require: './cjs/index.js' } };
-delete distPackageJson.scripts;
-delete distPackageJson.devDependencies;
+const gitCommit = await Bun.$`git rev-parse HEAD`.text().catch(() => 'unknown');
+const distPackageJson: Record<string, unknown> = utils.object.pickKeys(packageJson, ['name', 'type', 'version', 'description', 'author', 'license']);
+distPackageJson.main = 'main.js';
+distPackageJson.gitCommit = gitCommit.trim();
 
 const distPackageJsonString = JSON.stringify(distPackageJson, null, 2);
-fs.writeFileSync(`${distDir}/package.json`, distPackageJsonString);
+await Bun.write(`${distDir}/package.json`, distPackageJsonString);
 
 /** Copy supporting files into 'dist' */
 fs.copyFileSync(`${rootDir}/README.md`, `${distDir}/README.md`);
 fs.copyFileSync(`${rootDir}/LICENSE`, `${distDir}/LICENSE`);
 
-{
-  /** Building ESM */
-  const esmDir = path.join(distDir, 'esm');
-  const tsc = ['tsc', '--outDir', esmDir, '--project', 'tsconfig.build.json'];
-  const tscAlias = ['tsc-alias', '--outDir', esmDir, '--project', 'tsconfig.build.json'];
-  let result = spawnSync('bunx', tsc, { cwd: rootDir, stdio: 'inherit' });
-  if (result.status === 0) result = spawnSync('bunx', tscAlias, { cwd: rootDir, stdio: 'inherit' });
-  if (result.status !== 0) error('ESM Build failed');
-}
-
-{
-  /** Building CJS */
-  const cjsDir = path.join(distDir, 'cjs');
-  const tsc = ['tsc', '--outDir', cjsDir, '--module', 'CommonJS', '--project', 'tsconfig.build.json'];
-  const tscAlias = ['tsc-alias', '--outDir', cjsDir, '--project', 'tsconfig.build.json'];
-  let result = spawnSync('bunx', tsc, { cwd: rootDir, stdio: 'inherit' });
-  if (result.status === 0) result = spawnSync('bunx', tscAlias, { cwd: rootDir, stdio: 'inherit' });
-  if (result.status !== 0) error('CJS Build failed');
-  fs.writeFileSync(`${cjsDir}/package.json`, JSON.stringify({ type: 'commonjs' }));
-}
-
-/** Removing temporary files */
-const tsbuildinfo = path.join(distDir, 'tsconfig.build.tsbuildinfo');
-if (fs.existsSync(tsbuildinfo)) fs.rmSync(tsbuildinfo);
+const entryPoint = path.join(rootDir, 'src', 'main.ts');
+const result = await Bun.build({ entrypoints: [entryPoint], target: 'bun', minify: { identifiers: false }, outdir: 'dist' });
+if (!result.success) error(`Build failed: ${result.logs.join('\n')}`);
 
 const endTime = process.hrtime(startTime);
 const timeTaken = endTime[0] * 1e3 + endTime[1] * 1e-6;
-success(`Built successfull in ${formatTime(timeTaken)}`);
+success(`Built successful in ${formatTime(timeTaken)}`);
