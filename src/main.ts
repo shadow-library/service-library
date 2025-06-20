0d39e366644a02e18900dd61a8fbcdcbb4fbbe0f
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2025-06-20 20:31:32 +0400

    feat: fetch git commit from package.json (#4)

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import 'reflect-metadata';
 /**
  * Importing npm packages
  */
+import path from 'node:path';
+
 import { ShadowFactory } from '@shadow-library/app';
 import { Logger } from '@shadow-library/common';
 
@@ -21,7 +23,14 @@ import { AppModule } from './app.module';
 /**
  * Declaring the constants
  */
-const gitCommit = process.env.GIT_COMMIT_SHA || 'unknown';
+const packageJsonPath = path.join(import.meta.dirname, 'package.json');
+const packageJsonFile = Bun.file(packageJsonPath);
+
+let gitCommit = '-';
+if (await packageJsonFile.exists()) {
+  const packageJson = await packageJsonFile.json();
+  if (packageJson.gitCommit) gitCommit = packageJson.gitCommit;
+}
 
 Logger.setDefaultMetadata({ gitCommit }).addDefaultTransports();
 ShadowFactory.create(AppModule).then(app => app.start());
