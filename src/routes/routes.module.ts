49468d85361f890f465dc1682c4a0ea82f76cad0
Leander Paul <leanderpaul97@outlook.com>
2025-11-03 19:08:45 +0400

    feat: include http core module

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -1,17 +1,13 @@
 /**
  * Importing npm packages
  */
-import cookie from '@fastify/cookie';
 import { Config } from '@shadow-library/common';
 import { FastifyModule } from '@shadow-library/fastify';
-import { HealthModule } from '@shadow-library/modules';
-import { OpenAPIV3 } from 'openapi-types';
+import { HttpCoreModule } from '@shadow-library/modules';
 
 /**
  * Importing user defined packages
  */
-
-import { RequestInitializerMiddleware } from './request-initializer.middleware';
 
 /**
  * Defining types
@@ -20,36 +16,10 @@ import { RequestInitializerMiddleware } from './request-initializer.middleware';
 /**
  * Declaring the constants
  */
-const openapi: Partial<OpenAPIV3.Document> = {
-  info: { title: Config.get('app.name'), version: '1.0.0' },
-  components: {},
-};
 
 export const HttpRouteModule = FastifyModule.forRoot({
-  imports: [HealthModule],
-  controllers: [RequestInitializerMiddleware],
+  imports: [HttpCoreModule.forRoot()],
 
   host: Config.get('server.host'),
   port: Config.get('server.port'),
-  fastifyFactory: async instance => {
-    await instance.register(cookie);
-
-    if (Config.isDev()) {
-      const fastifySwagger = await import('@fastify/swagger');
-      const scalar = await import('@scalar/fastify-api-reference');
-
-      await instance.register(fastifySwagger, { openapi });
-      await instance.register(scalar.default, { routePrefix: '/dev/api-docs' });
-    }
-
-    if (Config.isProd()) {
-      const helmet = await import('@fastify/helmet');
-      const compress = await import('@fastify/compress');
-
-      await instance.register(helmet, { global: true });
-      await instance.register(compress, { global: true });
-    }
-
-    return instance;
-  },
 });
