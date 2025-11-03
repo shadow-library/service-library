86211adbab1656f0ffb3848b320d61a61c637f15
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2025-11-03 19:09:54 +0400

    feat: include http core module (#11)

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,7 @@ import './bootstrap';
 /**
  * Importing npm packages
  */
-import { Module, OnModuleInit } from '@shadow-library/app';
-import { Logger } from '@shadow-library/common';
-import { ContextService } from '@shadow-library/fastify';
+import { Module } from '@shadow-library/app';
 
 /**
  * Importing user defined packages
@@ -26,22 +24,4 @@ import { HttpRouteModule } from './routes';
 @Module({
   imports: [HttpRouteModule],
 })
-export class AppModule implements OnModuleInit {
-  constructor(private readonly contextService: ContextService) {}
-
-  private getLogMetadata(): Record<string, any> {
-    const metadata: Record<string, any> = {};
-
-    if (this.contextService.isInitialized()) {
-      const request = this.contextService.getRequest();
-      metadata.rid = request.id;
-      metadata.cid = request.cid;
-    }
-
-    return metadata;
-  }
-
-  onModuleInit(): void {
-    Logger.setLogMetadataProvider(() => this.getLogMetadata());
-  }
-}
+export class AppModule {}
